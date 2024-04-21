252b4c814561619a95fece08a93976618f729a88
Miguel Jiménez <mike0vortex@gmail.com>
2024-04-20 18:03:40 -0600

    postgressql config added

---
+++ src/helpers/postgressql-config.js
@@ -0,0 +1,11 @@
+const config = {
+
+    connectionLimit: 100,
+    database: 'melp_ixwm',
+    host: 'coi4ohv79t8c738bh2h0-a.virginia-postgres.render.com',
+    user: 'melp_ixwm_user',
+    debug: 'false'
+
+};
+
+module.exports = config;
